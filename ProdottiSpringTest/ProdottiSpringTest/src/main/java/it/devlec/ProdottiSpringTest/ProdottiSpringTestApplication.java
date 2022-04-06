75f5bd99595c32dadfecc47ec537a64f09d98a06
giorgioA00 <gaccogli@studenti.apuliadigitalmaker.it>
2022-04-06 15:15:36 +0200

    Implementazione Classi Repository, RestController, Inserimento, NotFound e SpringSecurity

---
+++ ProdottiSpringTest/ProdottiSpringTest/src/main/java/it/devlec/ProdottiSpringTest/ProdottiSpringTestApplication.java
@@ -0,0 +1,19 @@
+package it.devlec.ProdottiSpringTest;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProdottiSpringTestApplication {
+
+	private static final Logger logger = LoggerFactory.getLogger(ProdottiSpringTestApplication.class);
+	public static void main(String[] args) {
+
+		SpringApplication.run(ProdottiSpringTestApplication.class, args);
+		logger.info("Inizio della mia App spring");
+	}
+
+
+}
