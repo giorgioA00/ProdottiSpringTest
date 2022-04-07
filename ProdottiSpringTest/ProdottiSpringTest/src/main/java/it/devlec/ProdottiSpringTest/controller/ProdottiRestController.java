b47c51f4e8215324d033940c16390f6ea944a538
giorgioA00 <gaccogli@studenti.apuliadigitalmaker.it>
2022-04-07 10:55:23 +0200

    Implementazione metodi find

--- ProdottiSpringTest/ProdottiSpringTest/src/main/java/it/devlec/ProdottiSpringTest/controller/ProdottiRestController.java
+++ ProdottiSpringTest/ProdottiSpringTest/src/main/java/it/devlec/ProdottiSpringTest/controller/ProdottiRestController.java
@@ -1,6 +1,7 @@
 package it.devlec.ProdottiSpringTest.controller;
 
 
+import it.devlec.ProdottiSpringTest.ProdottiSpringTestApplication;
 import it.devlec.ProdottiSpringTest.avviso.ProdottoNonTrovato;
 import it.devlec.ProdottiSpringTest.model.Prodotto;
 import it.devlec.ProdottiSpringTest.persistence.ProdottiRepository;
@@ -14,7 +15,10 @@ import java.util.Date;
 import java.util.List;
 
 @RestController
+
 public class ProdottiRestController {
+    private static final Logger logger = LoggerFactory.getLogger(ProdottiSpringTestApplication.class);
+
     private ProdottiRepository repository;
 
     ProdottiRestController(ProdottiRepository repository) {
@@ -30,11 +34,15 @@ public class ProdottiRestController {
     public Prodotto trovaProdottoConID(@PathVariable Long id) {
         return repository.findById(id).orElseThrow(() -> new ProdottoNonTrovato(id));
     }
-    /*
-    @GetMapping("/prodotto/{nome}")
-    public Prodotto trovaProdottoConNome(@PathVariable String nome) {
-        return repository.findBynome(nome).orElseThrow(() -> new ProdottoNonTrovato(nome));
-    }*/
+
+    @GetMapping("/prodotto/ricercapernome")
+    public List<Prodotto> trovaProdottoConNome(@RequestParam String nome) {
+        return  repository.findByNome(nome);
+    }
+    @GetMapping("/prodotto/quantita")
+    public List<Prodotto> trovaProdottoConQuantita(@RequestParam float quantita) {
+        return repository.findAllByquantita(quantita);
+    }
 
     @PostMapping("/prodotto")
     public Prodotto inserisciNuovoProdotto(@RequestBody Prodotto nuovoProdotto) {
@@ -49,18 +57,48 @@ public class ProdottiRestController {
     @DeleteMapping("/prodotto/{id}")
     void eliminaProdotto(@PathVariable Long id){
         repository.deleteById(id);
-    }/*
+    }
+    /*
     @GetMapping("/prodotti/ricercaprezzo")
     public List<Prodotto> ricercaPerPrezzo(@RequestParam(name="min") float min,
                                        @RequestParam(name="max") float max){
         return repository.findByprezzoBetween(min,max);
-    }
-    @GetMapping("/prodotti/ricercadataregistrazione")
-    public List<Prodotto> ricercaPerDataDiRegistrazione(@RequestParam(name="datada") @DateTimeFormat(pattern= "dd-MM-yyyy")
-                                                              Date datada,
-                                                      @RequestParam(name="dataa") @DateTimeFormat(pattern= "dd-MM-yyyy")
-                                                              Date dataa){
-        return repository.findBydataBetween(datada,dataa);
     }*/
+    @GetMapping("/prodotti/ricercaprezzo")
+    public List<Prodotto> ricercaPerPrezzo(@RequestParam(name = "min") float min,
+                                           @RequestParam(name = "max") float max) {
+        return repository.findByprezzoBetween(min, max);
+    }
+    @GetMapping("/prodotti/quantitainferiore")
+    public List<Prodotto> ricercaPerQuantitaInferirore(@RequestParam(name = "max") float max) {
+        return repository.findByquantitaLessThan(max);
+    }
+
+    @GetMapping("/ordinamento")
+    public List<Prodotto> ricercaOrdinata() {
+        return repository.findByOrderByQuantita();
+    }
+    @GetMapping("/prodotti/ricercadata")
+    public List<Prodotto> ricercaPerDataDiAcquisto(@RequestParam(name = "data") @DateTimeFormat(pattern = "dd-MM-yyyy") Date data){
+        return repository.findBydataacquisto(data);
+    }
+    @GetMapping("/prodotti/ricercadataacquisto")
+    public List<Prodotto> ricercaPerDataDiAcquisto(@RequestParam(name = "datda") @DateTimeFormat(pattern = "dd-MM-yyyy")
+                                                   Date datada,
+                                                   @RequestParam(name = "dataa") @DateTimeFormat(pattern = "dd-MM-yyyy")
+                                                   Date dataa){
+        return repository.findBydataacquistoBetween(datada, dataa);
+    }
+
+    @PostMapping("/caricafile")
+    public String caricaFile(@RequestParam("file") MultipartFile file){
+
+        String infoFile = file.getOriginalFilename() + " " + file.getContentType();
+        String conFormat = String.format("%s-%s", file.getOriginalFilename(), file.getContentType());
+        logger.info(infoFile);
+        logger.info(conFormat);
+
+        return conFormat;
+    }
 
 }
